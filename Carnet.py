28b3245ceca791e66da200a399fbe6f54d3f8a6f
fmoro1129 <fmoro1129@gmail.com>
2024-09-11 20:52:30 -0300

    Create Carnet.py

---
+++ Carnet.py
@@ -0,0 +1,11 @@
+nombre=input("ingrese tu nombre:")
+apellido=input("ingrese tu apellido:")
+edad=int(input("ingrese la edad que tiene:"))
+if edad==18:
+    print("solo puede manejar en la ciudad.")
+elif edad >18 and edad<21 :
+    print("usted puede conducir en la colectora.")
+elif edad >= 21:
+   print ("Libre de conduccion")
+elif edad <18 :
+    print("Usted no puede conducir")
